be16521a37b3486e9feaf1e7ec38058746bb5ed6
Peter Cai <peter@typeblog.net>
2019-04-05 16:44:18 +0200

    Initial support for in-display fingerprint sensors
    * Initially based upon phh's Mi9 implementation,
      however a lot of changes have been made since.
    * All device specific stuff has been moved to
      IFingerprintInscreen HIDL interface and most
      of code has been improved and refactored.
    
    AOSiP edit: remove all ties to LineageContextConstants.Features.FOD as we dont want no damn sdk stuffs
    
    Change-Id: Ia905ee8b2e828e0d34ef8c01a41717323dcc220f
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- services/core/java/com/android/server/statusbar/StatusBarManagerService.java
+++ services/core/java/com/android/server/statusbar/StatusBarManagerService.java
@@ -729,6 +729,29 @@ public class StatusBarManagerService extends IStatusBarService.Stub implements D
             try {
                 mBar.killForegroundApp();
             } catch (RemoteException ex) {
+                // do nothing
+	    }
+	}
+    }
+
+    @Override
+    public void showInDisplayFingerprintView() {
+        if (mBar != null) {
+            try {
+                mBar.showInDisplayFingerprintView();
+            } catch (RemoteException ex) {
+                // do nothing
+            }
+        }
+    }
+
+    @Override
+    public void hideInDisplayFingerprintView() {
+        if (mBar != null) {
+            try {
+                mBar.hideInDisplayFingerprintView();
+            } catch (RemoteException ex) {
+                // do nothing
             }
         }
     }
