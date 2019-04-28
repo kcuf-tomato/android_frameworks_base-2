dc3d5185f330e5f94eccd30b6aacd9f69eaf1e84
Alex <alex@dirtyunicorns.com>
2019-04-27 21:10:29 -0300

    Introduce lockscreen date styles [1/2]
    Blissroms edit [rohanpurohit] : adapt to tuner
    
    Change-Id: Iac0620507730ce8e465d142590d4ed19cd141a88
    Signed-off-by: rohan <purohit.rohan@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5822,7 +5822,14 @@ public final class Settings {
         public static final String VIBRATE_ON_NOTIFICATIONS = "vibrate_on_notifications";
 
         /**
+         * Select which lockscreen date style to display
+         * @hide
+         */
+        public static final String LOCKSCREEN_DATE_SELECTION = "lockscreen_date_selection";
+
+        /**
          * Whether to pulse ambient on new music tracks
+         *
          * @hide
          */
         public static final String PULSE_ON_NEW_TRACKS = "pulse_on_new_tracks";
