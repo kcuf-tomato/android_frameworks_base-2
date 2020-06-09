330556a908869fd306e09f819e6696d3a5589c70
vjspranav <pranavasri@live.in>
2020-06-09 16:13:12 +0200

    Revert "Ambient music: Pulse on new tracks even if all Ambient options are off"
    This reverts commit a5d8acb5f73c4d1afa06f3609e2c342fe298ff61.

--- core/java/android/hardware/display/AmbientDisplayConfiguration.java
+++ core/java/android/hardware/display/AmbientDisplayConfiguration.java
@@ -53,8 +53,7 @@ public class AmbientDisplayConfiguration {
                 || wakeDisplayGestureEnabled(user)
                 || pickupGestureEnabled(user)
                 || tapGestureEnabled(user)
-                || doubleTapGestureEnabled(user)
-                || isAmbientTickerEnabled(user);
+                || doubleTapGestureEnabled(user);
     }
 
     /** {@hide} */
@@ -237,15 +236,5 @@ public class AmbientDisplayConfiguration {
 
     private boolean boolSetting(String name, int user, int def) {
         return Settings.Secure.getIntForUser(mContext.getContentResolver(), name, def, user) != 0;
-    }
-
-    public boolean isAmbientGestureEnabled(int user) {
-        return Settings.System.getIntForUser(mContext.getContentResolver(),
-                Settings.System.DOZE_TRIGGER_DOUBLETAP, 1, user) != 0;
-    }
-
-    public boolean isAmbientTickerEnabled(int user) {
-        return Settings.System.getIntForUser(mContext.getContentResolver(),
-                Settings.System.PULSE_ON_NEW_TRACKS, 1, user) != 0;
     }
 }
