1c8d612e2b4c69eeab43a81da130af1d280f33a7
vjspranav <pranavasri@live.in>
2020-06-14 12:47:39 +0200

    Revert "base: Double tap to trigger doze [1/2]"
    This reverts commit 0ad7a3e105365c98e6b240c71fe0c854a1718563.

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -171,7 +171,6 @@ import android.service.dreams.IDreamManager;
 import android.service.vr.IPersistentVrStateCallbacks;
 import android.speech.RecognizerIntent;
 import android.telecom.TelecomManager;
-import android.text.TextUtils;
 import android.util.Log;
 import android.util.LongSparseArray;
 import android.util.MutableBoolean;
@@ -414,10 +413,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private boolean mVolumeMusicControl;
     private boolean mVolumeWakeActive;
 
-    // Double-tap-to-doze
-    private boolean mDoubleTapToWake;
-    private boolean mDoubleTapToDoze;
-    private boolean mNativeDoubleTapToDozeAvailable;
     ANBIHandler mANBIHandler;
     private boolean mANBIEnabled;
 
@@ -876,9 +871,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     UserHandle.USER_ALL);
 	    resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.THREE_FINGER_GESTURE), false, this,
-                    UserHandle.USER_ALL);
-            resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.DOZE_TRIGGER_DOUBLETAP), false, this,
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.Secure.getUriFor(
                     Settings.Secure.TORCH_POWER_BUTTON_GESTURE), false, this,
@@ -1918,10 +1910,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         mAccessibilityShortcutController =
                 new AccessibilityShortcutController(mContext, new Handler(), mCurrentUserId);
         mLogger = new MetricsLogger();
-
-        // Double-tap-to-doze
-        mNativeDoubleTapToDozeAvailable = !TextUtils.isEmpty(
-                mContext.getResources().getString(R.string.config_dozeDoubleTapSensorType));
         // Init display burn-in protection
         boolean burnInProtectionEnabled = context.getResources().getBoolean(
                 com.android.internal.R.bool.config_enableBurnInProtection);
@@ -2234,13 +2222,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     public void updateSettings() {
         ContentResolver resolver = mContext.getContentResolver();
         boolean updateRotation = false;
-
-        // Double-tap-to-doze
-        mDoubleTapToWake = Settings.Secure.getInt(resolver,
-                Settings.Secure.DOUBLE_TAP_TO_WAKE, 0) == 1;
-        mDoubleTapToDoze = Settings.System.getInt(resolver,
-                Settings.System.DOZE_TRIGGER_DOUBLETAP, 0) == 1;
-
         synchronized (mLock) {
             mEndcallBehavior = Settings.System.getIntForUser(resolver,
                     Settings.System.END_BUTTON_BEHAVIOR,
@@ -4435,15 +4416,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
             case KeyEvent.KEYCODE_WAKEUP: {
                 result &= ~ACTION_PASS_TO_USER;
-                // Double-tap-to-doze
-                if (mDoubleTapToWake && mDoubleTapToDoze && !mNativeDoubleTapToDozeAvailable) {
-                    isWakeKey = false;
-                    if (!down) {
-                        mContext.sendBroadcast(new Intent("com.android.systemui.doze.pulse"));
-                    }
-                } else {
-                    isWakeKey = true;
-                }
+                isWakeKey = true;
                 break;
             }
 
