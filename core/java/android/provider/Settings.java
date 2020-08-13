25c994d1f189b1f535191cde40790214687f721d
vjspranav <pranavasri@live.in>
2020-08-13 17:24:26 +0200

    Revert "SystemUI: Add new FOD pressed state icons [1/2]"
    This reverts commit 90300c3238fe37722e879745b071d4db6a599931.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5884,7 +5884,7 @@ public final class Settings {
 
         /** @hide */
         private static final Validator FOD_PRESSED_STATE_VALIDATOR =
-                new SettingsValidators.InclusiveIntegerRangeValidator(0, 6);
+                new SettingsValidators.InclusiveIntegerRangeValidator(0, 2);
 
         /**
          * FOD recognizing animation picker
